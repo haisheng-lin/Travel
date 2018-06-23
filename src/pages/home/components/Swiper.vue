945c2e26e97c58c609af26fbf31b92194e65f4b7
haisheng-lin <hlin86@asu.edu>
2018-06-23 23:53:42 +0800

    index finish

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="wrapper">
-    <swiper :options="swiperOption">
+    <swiper :options="swiperOption" v-if="showSwiper">
       <!-- slides -->
-      <swiper-slide v-for="swiper of swiperList" :key="swiper.id">
-        <img class="swiper-img" :src="swiper.imgUrl" />
+      <swiper-slide v-for="item of list" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" />
       </swiper-slide>
       <!-- Optional controls -->
       <div class="swiper-pagination" slot="pagination"></div>
@@ -14,20 +14,21 @@
 <script>
 export default {
   name: 'HomeSwiper',
+  props: {
+    list: Array,
+  },
   data() {
     return {
       swiperOption: {
         pagination: '.swiper-pagination',
         loop: true,
       },
-      swiperList: [{
-        id: '0001',
-        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/13/6332699c44387902.jpg_750x200_b7afa4f8.jpg',
-      }, {
-        id: '0002',
-        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/3b/ef86879aa50e3002.jpg_750x200_2a108508.jpg',
-      }],
     };
+  },
+  computed: {
+    showSwiper() {
+      return this.list.length;
+    },
   },
 };
 </script>
